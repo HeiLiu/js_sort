a9e982e6c13ac223576f8fc740f55cd19039be25
HeiLiu <969403750@qq.com>
2018-08-02 00:17:53 +0800

    QuickSort

---
+++ sourceCode/quickSort.js
@@ -0,0 +1,21 @@
+function QuickSort (arr) {
+  if(arr.length <= 1) {
+    return arr
+  }
+  // 找一个基准点  取中间的数
+  var num = Math.floor(len/2)
+  // 从数组中把基准点拿出来
+  var numValue = arr.splice(num, 1)
+  var left = [], right =[]
+  for(var i = 0, len = arr.length; i< len; i++ ) {
+    if(arr[i] < numValue) {
+      left.push(arr[i])
+    } else {
+      right.push(arr[i])
+    }
+  }
+  // 返回一个递归 并且把每次排好序的两部分和基准点拼接
+  return QuickSort(left).concat(numValue, QuickSort(right))
+}
+
+console.log(QuickSort([12, 4, 76, 34, 64, 33, 22]))
